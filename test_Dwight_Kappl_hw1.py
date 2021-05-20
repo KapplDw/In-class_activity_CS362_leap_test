c331be0157a2d31da0e0cd5833a1346704e7a857
Dwight Kappl <55997890+KapplDw@users.noreply.github.com>
2021-05-20 16:56:32 -0700

    Was not sure if I needed to add this file from pytest

---
+++ test_Dwight_Kappl_hw1.py
@@ -0,0 +1,61 @@
+#################### PLAN ##########################
+# Leap Year determiner
+
+# Takes in year from user
+
+# Checks if divisible by 4 if not its not
+
+# Checks if divisible by 100 if not it is
+
+# Checks if divisible by 400 if not its not
+
+# Ask if they would like to test another year
+
+################# FUNCTIONS #########################
+def test_year_case():
+    assert leap(2021) == False 
+    assert leap(2020) == True
+    assert leap(1983) == False
+
+def whole_Number(x):
+    # Means Whole number
+    if (x - int(x) == 0):
+        return True
+    else:
+        return False
+
+
+def leap(year):
+    # Main code
+    
+        # Converts string year to an int
+    year = int(year)
+        # Checks if divisible by 4
+    temp = year / 4.0
+    if not whole_Number(temp):
+        return False
+        # Checks if divisible by 100
+    temp = year / 100.0
+    if not whole_Number(temp):
+        return True
+            
+        # Checks if divisible by 400
+    temp = year / 400.0
+    if not whole_Number(temp):
+        return False
+            
+
+
+###################### MAIN CODE #############################
+#while True:
+    # Input loop and check
+   
+    year = input("Enter the year you would like to test: ")
+    
+    leap(year)
+
+    
+
+    # Replay option
+    ##if user != "y":
+        #break
